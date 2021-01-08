acf140942832b23eba1178247ba7dd3e2e6261e3
ivanlima45 <ivanlimasanto@gmail.com>
2021-01-08 00:27:33 -0300

    just added a comment2

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,6 @@
 # create new dictionary.
 phone_book = {"John": 123, "Jane": 234, "Jerard": 345}    # "John", "Jane" and "Jerard" are keys and numbers are values
-print(phone_book)
+print(phone_book) ################
 
 # Add new item to the dictionary
 phone_book["Jill"] = 345
